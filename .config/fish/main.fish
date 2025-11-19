320a532efabc5b3f91ce80a4086b4c4970c9f08a
Lucas Lamboley <lucas.lamboley@groupementor.com>
2025-11-19 16:00:38 +0100

    :sparkles: Add fish_update_completions

--- .config/fish/main.fish
+++ .config/fish/main.fish
@@ -12,6 +12,17 @@ function update-packages --description "Update and upgrade apt packages"
     sudo apt install -y && sudo apt upgrade -y && sudo apt clean && sudo apt autoremove
 end
 
+function lucas-dtag --description "Delete a tag from git"
+    if [[ "$#" -le 1 ]]; then
+        log_error "Missing arguments"
+        log_error "Usage: $0 <tag to delete>"
+        exit 1
+    fi
+
+	# git tag --delete $(CURRENTTAG)
+	# git push --delete origin $(CURRENTTAG)
+end
+
 set -U fish_greeting
 
 set -gx EDITOR nvim
