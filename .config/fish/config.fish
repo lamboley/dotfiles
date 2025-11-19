f0477c33e0fe5d8a07f9a76ceba459c28572e4b3
Lucas Lamboley <lucas.lamboley@groupementor.com>
2025-11-19 14:21:48 +0100

    :sparkles: Add fish_update_completions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,17 @@
-set fish_greeting ""
+set -U fish_greeting
 
+alias .. "cd .."
+alias ... "cd ../.."
 
 alias mkdir "mkdir -p"
-alias ls "ls -l"
+
+alias grep "grep --color=auto"
+
+alias ls='ls --color=auto -l'
 alias ll "ls -l"
+
 alias vim "nvim"
+
+alias sudo "sudo "
 
 starship init fish | source
