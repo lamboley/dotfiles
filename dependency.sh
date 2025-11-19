23cc9a4b13d2fefe7b3628cb881330bf1edd3012
Lucas Lamboley <lucas.lamboley@groupementor.com>
2025-11-19 13:49:32 +0100

    :sparkles: Add kitty.conf

--- dependency.sh
+++ dependency.sh
@@ -6,6 +6,6 @@ if ! [ $(id -u) = 0 ]; then
 fi
 
 apt update -y && apt upgrade -y
-apt install -y neovim fish tmux curl
+apt install -y neovim fish kitty curl
 
 curl -sS https://starship.rs/install.sh | sh
--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@
 rm -f "${HOME}/.bash_aliases"
 ln -s -f "${HOME}/.dotfiles/aliases" "${HOME}/.bash_aliases"
 
-mkdir -p "${HOME}/.tmux/plugins"
-git clone git@github.com:o0th/tmux-nova.git ${HOME}/.tmux/plugins/tmux-nova
-
-rm -f "${HOME}/.tmux.conf"
-ln -s -f "${HOME}/.dotfiles/tmux.conf" "${HOME}/.tmux.conf"
-
 mkdir -p "${HOME}/.config/fish"
 rm -f "${HOME}/.config/fish/config.fish"
 ln -s -f "${HOME}/.dotfiles/.config/fish/config.fish" "${HOME}/.config/fish/config.fish"
