894e71d2531f8bd87676cb97b73929f738d7c763
Lucas Lamboley <lucas.lamboley@groupementor.com>
2025-11-19 14:45:11 +0100

    :sparkles: Add fish_update_completions

--- .config/fish/main.fish
+++ .config/fish/main.fish
@@ -1,4 +1,12 @@
 # Author: Lucas Lamboley
 
+function fish_user_key_bindings
+    bind \cu update-dotfiles
+end
+
+function update-dotfiles --description "Updates my .dotfiles"
+    cd ~/.dotfiles && git pull && bash ./install.sh
+end
+
 # Disable fish welcome message
 set -U fish_greeting
