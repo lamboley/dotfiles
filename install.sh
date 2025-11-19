154e14a7f3e6023540d5980ac8b405cec7155f84
Lucas Lamboley <lucas.lamboley@groupementor.com>
2025-11-19 15:28:09 +0100

    :sparkles: Add fish_update_completions

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function log_info() {
-    echo -e "\033[0;103m[INFO]\033[0m --- $*"
+    echo -e "\033[0;103m[INFO]\033[0m $*"
 }
 
 log_info "Installs packages neovim, fish and curl."
@@ -9,21 +9,21 @@ sudo apt update -y && sudo apt install -y neovim fish curl
 
 command_kitty=$(command -v kitty)
 if [ ! "$command_kitty" ]; then
-    log_info "Installing kitty ..."
+    log_info "... Installing kitty"
     curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 else
-    log_info "Kitty already installed."
+    log_info "--- Kitty already installed."
 fi
 
 command_starship=$(command -v starship)
 if [ ! "$command_starship" ]; then
-    log_info "Installing starship ..."
+    log_info "... Installing starship"
     curl -sS https://starship.rs/install.sh | sh
 else
-    log_info "starship already installed."
+    log_info "--- starship already installed."
 fi
 
-log_info "Configuring fish ..."
+log_info "... Configuring fish"
 
 mkdir -p "${HOME}/.config/fish"
 rm -f "${HOME}/.config/fish/config.fish"
@@ -32,13 +32,13 @@ ln -s -f "${HOME}/.dotfiles/.config/fish/main.fish" "${HOME}/.config/fish/main.f
 ln -s -f "${HOME}/.dotfiles/.config/fish/aliases.fish" "${HOME}/.config/fish/aliases.fish"
 ln -s -f "${HOME}/.dotfiles/.config/fish/config.fish" "${HOME}/.config/fish/config.fish"
 
-log_info "Configuring kitty ..."
+log_info "... Configuring kitty"
 
 mkdir -p "${HOME}/.config/kitty"
 rm -f "${HOME}/.config/kitty/kitty.conf"
 ln -s -f "${HOME}/.dotfiles/.config/kitty/kitty.conf" "${HOME}/.config/kitty/kitty.conf"
 
-log_info "Configuring neovim ..."
+log_info "... Configuring neovim"
 
 rm -Rf "${HOME}/.config/nvim"
 git clone https://github.com/LazyVim/starter "${HOME}/.config/nvim"
