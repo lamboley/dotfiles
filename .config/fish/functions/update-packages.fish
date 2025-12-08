0c0e5b2d87270a73e5de8d7ed47589c4b0dff819
Lucas Lamboley <lucas.lamboley@groupementor.com>
2025-12-08 14:07:21 +0100

    :sparkles: Update instead of instakk

--- .config/fish/functions/update-packages.fish
+++ .config/fish/functions/update-packages.fish
@@ -1,3 +1,3 @@
 function update-packages --description 'Update and upgrade apt packages'
-    sudo apt install -y && sudo apt upgrade -y && sudo apt clean -y && sudo apt autoremove -y
+    sudo apt update -y && sudo apt upgrade -y && sudo apt clean -y && sudo apt autoremove -y
 end
