60c615e09f8f7744b5f40868dbeef7e8594fab53
lamboley <lucas.lamboley@outlook.com>
2026-03-26 21:50:22 +0100

    Update .wezterm.lua to set package path dynamically based on user home directory

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+local home = os.getenv("HOME") or os.getenv("USERPROFILE")
+package.path = home .. "/.dotfiles/wezterm/?.lua;" .. package.path
+
 require("config.appearance")(config)
 require("config.keys")(config)
 require("config.platform")(config)
