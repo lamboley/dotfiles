44f491304f0381ebf70df169320b87676db1fa16
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-03-26 13:32:31 +0100

    Adjust window background opacity and initial size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,11 +6,16 @@ local config = wezterm.config_builder()
 config.color_scheme = "Dracula (Official)"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 11
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.85
 
 -- Minimalisme
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+
+-- Size
+config.window_close_confirmation = "NeverPrompt"
+config.initial_cols = 160
+config.initial_rows = 45
 
 -- Keybindings
 config.keys = {
