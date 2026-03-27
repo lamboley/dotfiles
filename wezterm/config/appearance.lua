c8ac3e0a6ee7cd39042f5c73bbd3acf101869c24
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-03-27 17:03:09 +0100

    Update appearance.lua

--- wezterm/config/appearance.lua
+++ wezterm/config/appearance.lua
@@ -2,7 +2,7 @@ return function(config)
     config.color_scheme = "Dracula (Official)"
     config.font = require("wezterm").font("FiraCode Nerd Font Mono")
     config.font_size = 11
-    config.window_background_opacity = 0.75
+    config.window_background_opacity = 0.85
     config.hide_tab_bar_if_only_one_tab = true
     config.window_decorations = "RESIZE"
 end
