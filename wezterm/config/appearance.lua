b20b89503d7e82c58d7e9dcb7569ed2e2408f1f5
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-03-29 13:49:37 +0200

    Fix font configuration syntax in appearance.lua

--- wezterm/config/appearance.lua
+++ wezterm/config/appearance.lua
@@ -1,6 +1,12 @@
+local wezterm = require("wezterm")
+
+wezterm.on('update-right-status', function(window, pane)
+  window:set_right_status(wezterm.strftime(' %H:%M:%S '))
+end)
+
 return function(config)
     config.color_scheme = "Dracula (Official)"
-    config.font = require("wezterm").font("FiraCode Nerd Font Mono")
+    config.font = wezterm.font("FiraCode Nerd Font Mono")
     config.font_size = 11
     config.window_background_opacity = 0.85
     config.hide_tab_bar_if_only_one_tab = true
