04e788a616fc961a6ad6f27eea4a7baab9e0cab2
Lucas Lamboley <lucas.lamboley@groupementor.com>
2026-03-30 11:06:55 +0200

    Update appearance configuration and add cspell support

--- wezterm/config/appearance.lua
+++ wezterm/config/appearance.lua
@@ -9,6 +9,7 @@ return function(config)
     config.font = wezterm.font("FiraCode Nerd Font Mono")
     config.font_size = 11
     config.window_background_opacity = 0.85
-    config.hide_tab_bar_if_only_one_tab = true
+    config.hide_tab_bar_if_only_one_tab = false
+    config.use_fancy_tab_bar = false
     config.window_decorations = "RESIZE"
 end
