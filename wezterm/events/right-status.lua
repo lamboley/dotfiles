f62d03d4beb02cc22900e2cee1b32225cf660d76
Lucas Lamboley <lucas.lamboley@groupementor.com>
2026-06-05 16:35:08 +0200

    Remove wezterm

--- wezterm/events/right-status.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require('wezterm')
-
-wezterm.on('update-right-status', function(window, _pane)
-   window:set_right_status(wezterm.format({
-      { Foreground = { Color = '#fab387' } },
-      { Text = wezterm.strftime('%a %H:%M:%S') .. ' ' },
-   }))
-end)
--- wezterm/utils/platform.lua
+++
@@ -1,8 +0,0 @@
-local t = require('wezterm').target_triple
-
-return {
-   os       = t:find('windows') and 'windows' or t:find('linux') and 'linux' or 'mac',
-   is_win   = t:find('windows') ~= nil,
-   is_linux = t:find('linux') ~= nil,
-   is_mac   = t:find('apple') ~= nil,
-}
