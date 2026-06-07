8112a7ce790c2a2386ee6420ded2cf9c89ed074d
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-06-07 13:43:59 +0200

    Configure Snacks plugin with key mappings
    Added key mappings for Snacks plugin functionalities.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,13 +1,29 @@
+-- ~/.config/nvim/lua/plugins/snacks.lua
+-- Snacks est une collection de modules. Hors LazyVim, on doit activer
+-- explicitement ceux qu'on veut et définir nos propres raccourcis.
+
 return {
   "folke/snacks.nvim",
+  priority = 1000, -- chargé tôt
+  lazy = false,    -- toujours disponible (l'explorer doit pouvoir s'ouvrir)
   opts = {
+    -- Modules activés (enabled = true). Sans ça, ils ne tournent pas.
+    explorer = { enabled = true },
     picker = {
+      enabled = true,
       sources = {
         explorer = {
-          hidden = true, -- show dotfiles (.git, .gitignore…)
-          ignored = true, -- show gitignored files
+          hidden = true,  -- affiche les dotfiles (.git, .gitignore…)
+          ignored = true, -- affiche les fichiers git-ignored
         },
       },
     },
+  },
+  -- Raccourcis (équivalents de ce que LazyVim définissait pour toi).
+  keys = {
+    { "<leader>e", function() Snacks.explorer() end, desc = "Explorateur de fichiers" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Chercher des fichiers" },
+    { "<leader>fg", function() Snacks.picker.grep() end, desc = "Rechercher dans les fichiers (grep)" },
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Chercher dans les buffers" },
   },
 }
