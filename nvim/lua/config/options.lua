eebd35485541a6e463e352e32a348618f4a77733
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-06-07 14:21:59 +0200

    Refactor Neovim options for clarity and consistency

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,36 +1,33 @@
--- ~/.config/nvim/lua/config/options.lua
--- Réglages de base de l'éditeur. `vim.opt` est l'équivalent Lua de `:set`.
-
 local opt = vim.opt
 
--- Numéros de ligne (absolu + relatif pour les sauts type 5j / 3k)
+-- Numéros de ligne
 opt.number = true
 opt.relativenumber = true
 
--- Indentation : 2 espaces, pas de tabs
+-- Indentation
 opt.expandtab = true
 opt.shiftwidth = 2
 opt.tabstop = 2
 opt.smartindent = true
 
--- Recherche : insensible à la casse, sauf si majuscule présente
+-- Recherche insensible à la casse
 opt.ignorecase = true
 opt.smartcase = true
 
 -- Confort visuel
-opt.wrap = false              -- pas de retour à la ligne automatique
-opt.signcolumn = "yes"        -- colonne des signes toujours là (évite le saut)
-opt.scrolloff = 8             -- garde 8 lignes de contexte au-dessus/dessous
-opt.termguicolors = true      -- vraies couleurs (truecolor)
-opt.cursorline = true         -- surligne la ligne courante
+opt.wrap = false         -- pas de retour à la ligne automatique
+opt.signcolumn = "yes"   -- colonne des signes toujours là
+opt.scrolloff = 8        -- garde 8 lignes de contexte au-dessus/dessous
+opt.termguicolors = true -- vraies couleurs
+opt.cursorline = true    -- surligne la ligne courante
 
 -- Comportement
-opt.mouse = "a"               -- souris active (utile sur tablette)
+opt.mouse = "a"               -- souris active
 opt.clipboard = "unnamedplus" -- partage le presse-papier système
 opt.undofile = true           -- historique d'annulation persistant
 opt.splitright = true         -- nouveaux splits verticaux à droite
 opt.splitbelow = true         -- nouveaux splits horizontaux en bas
 
--- Réactivité (utile pour Esc en terminal, comme on l'a vu)
+-- Réactivité
 opt.timeoutlen = 300
 opt.updatetime = 250
