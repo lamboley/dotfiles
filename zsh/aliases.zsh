bc73d0f3758cdf5c028dec98adc498bbe0aa0602
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-06-07 14:32:20 +0200

    Clean up zsh aliases and remove comments
    Removed comments and simplified alias definitions.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,16 +1,12 @@
-# Éditeur
 alias vim='nvim'
 
-# sshm : ne lister que mes hôtes
 alias sshm='sshm -c ~/.ssh/config'
 
-# Fichiers
 alias mkdir='mkdir -p'
 alias ls='eza --icons'
 alias ll='eza -la --icons --git'
 alias tree='eza --tree --icons'
 alias grep='grep --color=auto'
 
-# L'espace final permet d'utiliser un alias juste après sudo / watch
 alias sudo='sudo '
 alias watch='watch '
