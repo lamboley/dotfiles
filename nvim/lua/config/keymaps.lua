e21ef00432bb00e321d92bd89b8c1790b7953770
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-06-07 14:08:43 +0200

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,11 +10,12 @@ map("i", "jk", "<Esc>", { desc = "Sortir du mode insertion" })
 -- Effacer la surbrillance de recherche avec Échap en mode normal
 map("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Effacer la surbrillance" })
 
--- Navigation entre splits avec Ctrl + h/j/k/l
-map("n", "<C-h>", "<C-w>h", { desc = "Aller au split de gauche" })
-map("n", "<C-j>", "<C-w>j", { desc = "Aller au split du bas" })
-map("n", "<C-k>", "<C-w>k", { desc = "Aller au split du haut" })
-map("n", "<C-l>", "<C-w>l", { desc = "Aller au split de droite" })
+-- Navigation entre splits avec <leader> + h/j/k/l
+-- (Ctrl+h/j/k/l est intercepté par Zellij pour sa navigation entre panes)
+map("n", "<leader>h", "<C-w>h", { desc = "Aller au split de gauche" })
+map("n", "<leader>j", "<C-w>j", { desc = "Aller au split du bas" })
+map("n", "<leader>k", "<C-w>k", { desc = "Aller au split du haut" })
+map("n", "<leader>l", "<C-w>l", { desc = "Aller au split de droite" })
 
 -- Sauvegarde rapide
 map("n", "<leader>w", "<cmd>write<CR>", { desc = "Sauvegarder" })
