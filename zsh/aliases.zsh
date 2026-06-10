55ea8b25ef0b0ca11339916279be25060b7abcf4
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-06-10 19:06:29 +0200

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,12 +1,16 @@
-alias vim='nvim'
-
 alias sshm='sshm -c ~/.ssh/config'
 
 alias mkdir='mkdir -p'
-alias ls='eza --icons'
-alias ll='eza -la --icons --git'
-alias tree='eza --tree --icons'
 alias grep='grep --color=auto'
 
+# eza : seulement si présent, sinon ls/ll/tree restent les commandes standard
+if command -v eza >/dev/null 2>&1; then
+  alias ls='eza --icons'
+  alias ll='eza -la --icons --git'
+  alias tree='eza --tree --icons'
+else
+  alias ll='ls -la'
+fi
+
+# Le trailing space fait que le mot suivant est aussi expansé en alias
 alias sudo='sudo '
-alias watch='watch '
