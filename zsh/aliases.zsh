ac0277a5172cfd12edc3d12577fd34b43d0d4985
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-06-11 09:17:44 +0200

    Modify zsh aliases for improved command usage
    Update aliases for ls, ll, and lt based on eza availability.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,6 @@ alias sshm='sshm -c ~/.ssh/config'
 alias mkdir='mkdir -p'
 alias grep='grep --color=auto'
 
-# eza : seulement si présent, sinon ls/ll/tree restent les commandes standard
 if command -v eza >/dev/null 2>&1; then
   alias ls='eza --icons'
   alias ll='eza -la --icons --git'
@@ -14,5 +13,4 @@ else
   alias lt='ls -lrt'
 fi
 
-# Le trailing space fait que le mot suivant est aussi expansé en alias
 alias sudo='sudo '
