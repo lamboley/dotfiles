ec45916657886839c2f6be51c0d76cde632798d9
LAMBOLEY Lucas <lucas.lamboley@outlook.com>
2026-06-11 09:17:00 +0200

    Add 'lt' alias for listing files sorted by newest

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -7,9 +7,11 @@ alias grep='grep --color=auto'
 if command -v eza >/dev/null 2>&1; then
   alias ls='eza --icons'
   alias ll='eza -la --icons --git'
+  alias lt='eza -la --icons --sort newest'   # ls -lrt equivalent
   alias tree='eza --tree --icons'
 else
   alias ll='ls -la'
+  alias lt='ls -lrt'
 fi
 
 # Le trailing space fait que le mot suivant est aussi expansé en alias
