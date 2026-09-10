095f554a1253cadd95368ad053ebc2dc8e601f23
Lucas Lamboley <luas.lamboley@outlook.com>
2026-09-10 21:30:49 +0000

    feat: dd Ctrl+s for nvim to save file in edit

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,3 +4,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 vim.opt.number = true
+
+-- Ctrl-S sauvegarde, y compris depuis le mode insertion (on y reste).
+vim.keymap.set({ "n", "v" }, "<C-s>", "<Cmd>write<CR>")
+vim.keymap.set("i", "<C-s>", "<Cmd>write<CR>")
